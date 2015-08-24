6e9e5c9a24a65d1866b4d2b03ec2b837de5b5d60
Mitch Rivet & Micah Courey <student@epicodus.com>
2015-08-24 11:28:09 -0700

    add category update option

--- app/app.php
+++ app/app.php
@@ -6,16 +6,17 @@
     use Symfony\Component\Debug\Debug;
     Debug::enable();
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     $app = new Silex\Application();
 
     $app['debug'] = true;
 
-    $server = 'mysql:host=localhost;dbname=to_do_test';
+    $server = 'mysql:host=localhost;dbname=to_do';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
-
-
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
@@ -25,18 +26,9 @@
         return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
-    $app->get("/tasks", function() use ($app) {
-        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
-    });
-
-    $app->get("/categories", function() use ($app) {
-        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
-    });
-
-    $app->get("/categories/{id}", function($id) use ($app) {
-      $category = Category::find($id);
-      return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
-    });
+    // $app->get("/tasks", function() use ($app) {
+    //     return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    // });
 
     $app->post("/tasks", function() use ($app) {
         $description = $_POST['description'];
@@ -49,9 +41,10 @@
 
     });
 
-    $app->post("/delete_tasks", function() use ($app) {
-        Task::deleteAll();
-        return $app['twig']->render('tasks.html.twig');
+
+    $app->get("/categories/{id}", function($id) use ($app) {
+        $category = Category::find($id);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
 
     $app->post("/categories", function() use ($app) {
@@ -59,6 +52,26 @@
         $category->save();
         return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
+
+    $app->get("/categories/{id}/edit", function($id) use ($app) {
+      $category = Category::find($id);
+      return $app['twig']->render('category_edit.html.twig', array('category' => $category));
+    });
+
+    $app->patch("/categories/{id}", function($id) use ($app) {
+        $name= $_POST['name'];
+        $category = Category::find($id);
+        $category->update($name);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
+    });
+
+
+    $app->post("/delete_tasks", function() use ($app) {
+        Task::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+
 
     $app->post("/delete_categories", function() use ($app) {
         Category::deleteAll();
